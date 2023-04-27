37e06aa69b828806e8e591a67b51607d69439310
trendyjosh <trendyjosh@gmail.com>
2023-04-28 00:02:43 +0100

    Update todo for userLeft listener

--- src/listeners/userLeft.ts
+++ src/listeners/userLeft.ts
@@ -7,17 +7,10 @@ import { VoiceState } from "discord.js";
 })
 export class ChatInputCommandDenied extends Listener {
   run(client: SapphireClient, oldState: VoiceState, newState: VoiceState) {
-    // console.log(oldState, newState);
-    // console.log("ClientId: ", client.id ?? "");
-    // if (oldState.channel && client.id) {
-    //   console.log("Bot is here: ", oldState.channel.members.get(client.id));
-    // }
-    // if (oldState.channel !== null && oldState.guild.me.voice.channel !== null) {
-    //   if (!newState.channel || newState.channel.id !== oldState.guild.me.voice.channel.id) {
-    //     if (oldState.channel.members.size <= 1 && oldState.channel.id === oldState.guild.me.voice.channel.id) {
-    //     //   leave(oldState.guild.id);
-    //     }
-    //   }
-    // }
+    /**
+     * TODO
+     *
+     * Automatically disconnect bot if no users in voice channel.
+     */
   }
 }
